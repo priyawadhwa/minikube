4c430fea8e96d40fbee371e128c3c05466bb3ed9
Priya Wadhwa <priyawadhwa@google.com>
2020-06-10 16:29:36 -0700

    turn off metrics

--- pkg/minikube/bootstrapper/bsutil/ktmpl/v1beta2.go
+++ pkg/minikube/bootstrapper/bsutil/ktmpl/v1beta2.go
@@ -64,13 +64,9 @@ etcd:
   local:
     dataDir: {{.EtcdDataDir}}
     extraArgs:
+      enable-pprof: "true"
+      listen-client-urls: https://127.0.0.1:2379,https://{{.AdvertiseAddress}}:2379,http://127.0.0.1:2382
       listen-metrics-urls: ""
-controllerManager:
-  extraArgs:
-    "leader-elect": "false"
-scheduler:
-  extraArgs:
-    "leader-elect": "false"
 kubernetesVersion: {{.KubernetesVersion}}
 networking:
   dnsDomain: {{if .DNSDomain}}{{.DNSDomain}}{{else}}cluster.local{{end}}
